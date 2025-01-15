9a1e969701a33c6ecf3b670a40d44c2fba967e62
Yash singh <yk73690932@gmail.com>
2025-01-15 08:27:18 +0530

    adding build folder

---
+++ build/precache-manifest.deb41466fb9fba12b02ddfd4657772b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a26c8ea3824a2c42194fc53e07e7a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a313c183ac20e7e0fd01",
+    "url": "/static/css/main.0ff5a7a2.chunk.css"
+  },
+  {
+    "revision": "d5877e215803933b70ac",
+    "url": "/static/js/2.4373b640.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.4373b640.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61478671d53da608fa3e",
+    "url": "/static/js/3.66eba6ab.chunk.js"
+  },
+  {
+    "revision": "a313c183ac20e7e0fd01",
+    "url": "/static/js/main.7c560c7a.chunk.js"
+  },
+  {
+    "revision": "f7434117b581f70bab6b",
+    "url": "/static/js/runtime-main.2dd0d669.js"
+  },
+  {
+    "revision": "a6f125d68d7875d607dffcee808d3dbe",
+    "url": "/static/media/loading.a6f125d6.gif"
+  }
+]);
